a4be9cbc51a4a64ad8edbe91fcf5f673cc1c5dae
Alex Monteiro <eng.alexmonteiro@gmail.com>
2025-12-17 20:51:42 -0400

    Update dependences.sh
    Adicionando dependences

--- dependences.sh
+++ dependences.sh
@@ -1,1 +1,1 @@
-apt-get install libnm-util-dev libnm-glib-dev libacpi-dev libiw-dev libgps-dev liboping-dev
+apt-get install libnm-util-dev libnm-glib-dev libacpi-dev libiw-dev libgps-dev liboping-dev liboping-dev libbluetooth-dev libxcb-dpms0-dev libx11-dev libdbus-1-dev
